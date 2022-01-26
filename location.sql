d91e27e6295582a813ebbc2e267fab1e3ab4edec
Sumpfy <45922376+Sumpfy@users.noreply.github.com>
2022-01-26 12:30:03 +0100

    dasdas

--- location.sql
+++ location.sql
@@ -4,7 +4,7 @@ CREATE TABLE location
 ( id NUMBER(10) NOT NULL,
   PRIMARY KEY(id),
   map_id NUMBER(10) NOT NULL,
-  FOREIGN KEY(map_id) REFERENCES map(id),
+  FOREIGN KEY(custom_map_id) REFERENCES custom_map(id),
   latitude NUMBER(9,6) NOT NULL,
   longitude NUMBER(9,6) NOT NULL
 );
--- map.sql
+++ map.sql
@@ -1,12 +1,16 @@
+CREATE OR REPLACE DIRECTORY IMGDIR021 AS '/home/mschaibl/oracle';
+
 DROP INDEX myindex
+DROP TABLE custom_map
+DROP TABLE custom_map,custom_user,location;
 
-DROP TABLE map
-
-CREATE TABLE map
+CREATE TABLE custom_map
 ( id NUMBER(10) NOT NULL,
   PRIMARY KEY(id),
   title varchar2(100) NOT NULL,
   description varchar2(1000),
+  image ORDSYS.ORDImage,
+  image_sig ORDSYS.ORDImageSignature,
   hits NUMBER(38) DEFAULT 0,
   modified TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
   created TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
@@ -14,14 +18,47 @@ CREATE TABLE map
   FOREIGN KEY(custom_user_id) REFERENCES custom_user(id)
 );
 
-INSERT INTO map VALUES ( 1, 'My first Map', 'Diese Sammlung von Orten sollte sowohl Jung als auch Alt zum grübeln bringen. Viel Spaß mit diesem Geniestreich!' , DEFAULT, DEFAULT, DEFAULT, 2);
-INSERT INTO map VALUES ( 2, 'Die große Herausforderung' , 'Wer eine gehörige Portion Mut mitbringt, könnte diese große Herausforderung schaffen.', DEFAULT, DEFAULT, DEFAULT, 1);
-INSERT INTO map VALUES ( 3, 'Spanien', 'Weite Steppen, einige Bäume und vor allem eine Unmenge an kniffligen Orten warten in dieser Map auf euch.', DEFAULT, DEFAULT, DEFAULT, 2);
-INSERT INTO map VALUES ( 4, 'Leipzig', 'Ah ja, Leipzig. Warum auch nicht?', DEFAULT, DEFAULT, DEFAULT, 1);
+INSERT INTO custom_map VALUES ( 1, 'My first Map', 'Diese Sammlung von Orten sollte sowohl Jung als auch Alt zum grübeln bringen. Viel Spaß mit diesem Geniestreich!' , DEFAULT, DEFAULT, DEFAULT, 2);
+INSERT INTO custom_map VALUES ( 2, 'Die große Herausforderung' , 'Wer eine gehörige Portion Mut mitbringt, könnte diese große Herausforderung schaffen.', DEFAULT, DEFAULT, DEFAULT, 1);
+INSERT INTO custom_map VALUES ( 3, 'Spanien', 'Weite Steppen, einige Bäume und vor allem eine Unmenge an kniffligen Orten warten in dieser Map auf euch.', DEFAULT, DEFAULT, DEFAULT, 2);
+INSERT INTO custom_map VALUES ( 4, 'Leipzig', 'Ah ja, Leipzig. Warum auch nicht?', DEFAULT, DEFAULT, DEFAULT, 1);
 
-create index myindex on map(description) 
+create index myindex on custom_map(description) 
   indextype is ctxsys.context 
   parameters ('DATASTORE CTXSYS.DEFAULT_DATASTORE SYNC (ON COMMIT)');
 
-SELECT description FROM map WHERE CONTAINS ( description, 'Jung Alt' ) > 0;
-SELECT description FROM map WHERE CONTAINS ( description, 'Jung and Alt' ) > 0;
+SELECT description FROM custom_map WHERE CONTAINS ( description, 'Jung Alt' ) > 0;
+SELECT description FROM custom_map WHERE CONTAINS ( description, 'Jung and Alt' ) > 0;
+
+
+
+DECLARE
+-- application variables
+imageObj ORDSYS.ORDImage;
+ctx RAW(4000) := NULL;
+fileName VARCHAR(20);
+BEGIN
+FOR I IN 1..[ANZAHL DER ZU LADENDEN BILDER, z.B. 227] LOOP
+fileName := 'picture';
+IF I < 100 THEN
+fileName := fileName || '0';
+END IF;
+IF I < 10 THEN
+fileName := fileName || '0';
+END IF;
+INSERT INTO photos
+VALUES ( I, ORDSYS.ORDImage.init(),ORDSYS.ORDImageSignature.init() );
+-- Select the newly inserted row for update
+SELECT image INTO map FROM photos WHERE id = I for UPDATE;
+-- fill in the image BLOB.
+-- This example imports the image from the IMGDIR
+-- directory on a local file system
+-- (srcType=FILE) and automatically sets the properties.
+imageObj.setSource('file','IMGDIR021',fileName || I || '.jpg');
+imageObj.import(ctx);
+UPDATE photos SET image = imageObj WHERE id = I;
+END LOOP;
+COMMIT;
+-- Continue processing
+END;
+/
